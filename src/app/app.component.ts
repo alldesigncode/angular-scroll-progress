43633d46144f59bfa56549d5e6e112f86b906569
Beka Maisuradze <bekamais@Bekas-MacBook-Pro.local>
2021-01-24 15:11:46 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { DecimalPipe } from '@angular/common';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-scroll-progress';
+export class AppComponent implements OnInit {
+  public reachedTheEnd: boolean;
+
+  constructor(private decimalPipe: DecimalPipe) {}
+
+  ngOnInit() {
+    gsap.to('progress', {
+      value: 100,
+      scrollTrigger: {
+        trigger: '.main',
+        scrub: 0.3,
+        start: 'top top',
+        end: 'bottom bottom',
+        onUpdate: (options) => {
+          if (options instanceof ScrollTrigger) {
+            const value = Number(this.decimalPipe.transform(options.progress, '1.2-2'));
+            this.reachedTheEnd = value > 0.93;
+          }
+        }
+      },
+    });
+  }
+
+  onClick(elem: HTMLElement) {
+    elem.scrollIntoView({ behavior: 'smooth' });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DecimalPipe } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [DecimalPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
